191ddf6a6e95c8572d9c61bd4364821eabc157a4
niwhsam31 <149706865+niwhsam31@users.noreply.github.com>
2024-04-20 13:20:35 +0530

    Add files via upload

---
+++ rover.js
@@ -0,0 +1,19 @@
+let home_button = document.querySelector("#home");
+home_button.addEventListener("click",()=>{
+    window.location.href = "index.html"
+});
+
+let back_button = document.querySelector("#back_icon");
+back_button.addEventListener("click",()=>{
+    window.location.href = "index.html";
+});
+
+let about_us_button = document.querySelector("#about_us");
+about_us_button.addEventListener("click",()=>{
+    window.location.href = "about_us.html";
+});
+
+let technology_button = document.querySelector("#technologies_used");
+technology_button.addEventListener("click",()=>{
+    window.location.href = "technology.html";
+});
